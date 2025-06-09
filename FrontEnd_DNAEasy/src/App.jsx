8674478d1f2d64839ab850b126a1939ffc82a35b
Dương Văn Quyết <139792507+duongvanquyetd@users.noreply.github.com>
2025-06-09 08:07:01 +0700

    Update Booking Service Page

--- FrontEnd_DNAEasy/src/App.jsx
+++ FrontEnd_DNAEasy/src/App.jsx
@@ -6,6 +6,10 @@ import RegisterForm from './component/page/register';
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import PaternityBookingDetail from './component/page/PaternityBookingDetail';
+import { BookingServicePage } from './component/page/BookingService';
+import { VNPayReturn } from './component/page/VnpayReturn';
+import { YourAppointment } from './component/page/YourAppointment';
+import { HistoryBooking } from './component/page/HistoryBooking';
 
 function App() {
   return (
@@ -19,6 +23,11 @@ function App() {
         <Route path="/service/civil" element={<Service type="civil" />} />
         <Route path="/service/legal" element={<Service type="legal" />} />
         <Route path="/booking/paternity" element={<PaternityBookingDetail />} />
+        <Route path="/booking/:id" element={<BookingServicePage />} />
+        <Route path="/vnpay-callback" element={<VNPayReturn />} />
+        <Route path="/yourappoinment" element={<YourAppointment />} />
+        <Route path="/historyBooking" element={<HistoryBooking />} />
+
       </Routes>
     </BrowserRouter>
   );
--- FrontEnd_DNAEasy/src/service/service.js
+++ FrontEnd_DNAEasy/src/service/service.js
@@ -1,5 +1,8 @@
 import api from './api';
 
 export const getALlServies = () => {
-    return api.get('/services');
+    return api.get('/service');
+}
+export const getServiceById = (id) => {
+    return api.get(`/service/${id}`);
 }
