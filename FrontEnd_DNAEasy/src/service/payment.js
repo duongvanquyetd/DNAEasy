3adaad1483212edf20c76772e8ae80517ffbacb6
Ngoc Quang <Quangvn@gmail.com>
2025-07-03 22:38:14 +0700

    Them 2 danh sach chi tiet tien cho admin

--- FrontEnd_DNAEasy/src/service/payment.js
+++ FrontEnd_DNAEasy/src/service/payment.js
@@ -1,19 +1,29 @@
 import api from './api';
+
+export const GetPaymentList = (data) => {
+  return api.post("/payment/list", data);
+};
+
 export const GetPaymentStatus = (appointmentId ) => {
     return api.get(`payment/status/${appointmentId }`);
 }
+
 export  const PayToview = (appointmentId) => {
     return api.get(`/payment/paytoview/${appointmentId}`);
 }
+
 export const UpdatePaymentStatus = (appoinment) => {
     return api.post(`/payment/updateStatus`,appoinment);
 }
+
 export  const PayAgaint = (appointmentId) => {
     return api.get(`/payment/payagaint/${appointmentId}`);
 }
+
 export  const ConfirmPaidByCash = (appointmentId) => {
     return api.get(`/payment/confirmpaid/${appointmentId}`);
 }
+
 export const VNPay_Return = ()=>
 {
     return api.get("/payment/vnpay-callback");
@@ -22,6 +32,7 @@ export const VNPay_Return = ()=>
 export const  CreateRefund = (appoinment) =>{
     return api.post("payment/create",appoinment)
 }
+
 export const ChartOverview = (data)=>{
     return api.post("/appointment/revenue_chart",data)
 }
